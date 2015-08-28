74c58334677833cb9f041221069b55f385956bd7
yuo123 <aaa444777@gmail.com>
2015-08-28 13:24:08 +0300

    Worked on main form design

---
+++ CK2Editor/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CK2Editor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            saveSelector.SelectedIndex = 0;
+            fileChooser.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Paradox Interactive\Crusader Kings II\save games";
+        }
+
+        private void loadSaveButton_Click(object sender, EventArgs e)
+        {
+            if (fileChooser.ShowDialog() == DialogResult.OK)
+            {
+                saveSelector.Items.AddRange(fileChooser.FileNames);
+                saveSelector.SelectedIndex = saveSelector.Items.Count - 1;
+            }
+        }
+    }
+}
