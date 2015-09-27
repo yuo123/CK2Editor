a8511a73830ebf82c2813cc410456615c89e0d98
yuo123 <aaa444777@gmail.com>
2015-09-27 15:07:12 +0300

    Started work on XML-based format

--- CK2Editor/Editors/Editor.cs
+++ CK2Editor/Editors/Editor.cs
@@ -3,36 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
 
 namespace CK2Editor.Editors
 {
-    public abstract class Editor : IEditor
+    public class Editor : IEditor
     {
-        public virtual IDictionary<string, IEditor> GetSections()
+        public IDictionary<string, IEditor> GetSections()
         {
             throw new NotImplementedException();
         }
 
-        public virtual IDictionary<string, string> GetValues()
+        public IDictionary<string, string> GetValues()
         {
-            PropertyInfo[] props = this.GetType().GetProperties();
-            //filtered props
-            IEnumerable<PropertyInfo> fprops = props.Where(p => p.PropertyType == typeof(string) && p.CanRead && p.CanWrite && p.IsDefined(typeof(EditorValueAttribute)));
-            Dictionary<string, string> re = new Dictionary<string, string>(fprops.Count());
-            foreach (PropertyInfo prop in fprops)
-            {
-                re.Add(prop.Name, (string)prop.GetValue(this));
-            }
-            return re;
+            throw new NotImplementedException();
         }
 
-        public virtual void setValue(string name, string value)
+        public void setValue(string name, string value)
         {
-            PropertyInfo prop = this.GetType().GetProperty(name);
-            if ((!(prop.PropertyType == typeof(string))) || (!prop.CanWrite) || (!prop.IsDefined(typeof(EditorValueAttribute))))
-                throw new ArgumentException("Editor " + this.GetType().Name + "does not expose a valid value named " + name);
-            prop.SetValue(this, value);
+            throw new NotImplementedException();
         }
     }
 }
