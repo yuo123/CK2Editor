ef0494867db229829ef90c320d1e4d2d07a44b64
yuo123 <aaa444777@gmail.com>
2015-11-03 04:18:44 +0200

    Added previously untracked dependencies

---
+++ CK2EditorGUI/NodeControls/EntryValueNodeText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Aga.Controls.Tree.NodeControls;
+
+namespace CK2EditorGUI.NodeControls
+{
+    class EntryValueNodeText : NodeTextBox
+    {
+    }
+}
