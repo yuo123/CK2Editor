426d0e8c1fd6b37fa1cb4eb9c236ef5715baa7e5
yuo123 <aaa444777@gmail.com>
2016-07-10 20:13:48 +0300

    Introduced EntryGrouper

---
+++ CK2Editor/EntryGrouper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK2Editor
+{
+    public class EntryGrouper : SectionEntry
+    {
+        public override bool IsGrouper { get { return true; } }
+    }
+}
